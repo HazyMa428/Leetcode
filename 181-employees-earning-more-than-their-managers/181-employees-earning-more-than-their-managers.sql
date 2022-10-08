e7b1e13b4c948ee29b4111d0497cc91bf14c36ea
HazyMa428 <110778494+HazyMa428@users.noreply.github.com>
2022-10-08 13:01:56 -0400

    Time: 404 ms (75.49%), Space: 0B (100.00%) - LeetHub

--- 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -1,11 +1,11 @@
 # Write your MySQL query statement below
 Select 
-    a.Name AS 'Employee'
+    b.Name AS 'Employee'
 From
     Employee as a,
     Employee as b
 Where
-    a.ManagerId = b.Id
-        and a.salary > b.salary
+    a.ID = b.ManagerId
+        and a.salary < b.salary
 ;
     
