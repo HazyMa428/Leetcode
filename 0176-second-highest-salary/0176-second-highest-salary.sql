75cfdd837bb56160c298e8c01461eb7f55398a61
HazyMa428 <110778494+HazyMa428@users.noreply.github.com>
2022-10-11 07:33:46 -0400

    Time: 229 ms (84.72%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
 Select
-    Ifnull(
     (select distinct salary
     from employee
      order by salary desc
-    limit 1 offset 1), null) as SecondHighestSalary
+    limit 1 offset 1) as SecondHighestSalary
