9ff8fb6538798a228b4925925202ea0f87058fee
HazyMa428 <110778494+HazyMa428@users.noreply.github.com>
2022-11-03 08:41:06 -0400

    Time: 463 ms (91.27%), Space: 0B (100.00%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -2,7 +2,6 @@
 
 SELECT sell_date,
         COUNT(DISTINCT(product)) AS num_sold,
-        GROUP_CONCAT(DISTINCT (product)) AS products
+        GROUP_CONCAT(DISTINCT(product)) AS products
 FROM Activities
-GROUP BY sell_date;
-
+GROUP by sell_date;
