1acd91744e4710f17853a70e58d9faac2d52a637
HazyMa428 <110778494+HazyMa428@users.noreply.github.com>
2022-11-04 09:34:19 -0400

    Time: 523 ms (55.42%), Space: 0B (100.00%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -2,19 +2,18 @@
 
 SELECT id, 'Root' AS type
 FROM Tree
-WHERE p_id is NULL 
+WHERE p_id is NULL
 
 UNION
 
 SELECT id, 'Inner' AS type
 FROM Tree
-WHERE p_id is NOT NULL AND id in (SELECT p_id FROM Tree WHERE p_id is NOT NULL)
+WHERE p_id is NOT NULL and id in (SELECT DISTINCT p_id FROM Tree WHERE p_id is NOT NULL)
 
 UNION
 
 SELECT id, 'Leaf' AS type
 FROM Tree
-WHERE p_id is NOT NULL AND id NOT in (SELECT p_id FROM Tree WHERE p_id is NOT NULL)
-
+WHERE p_id is NOT NULL and id NOT in (SELECT DISTINCT p_id FROM Tree WHERE p_id is NOT NULL)
 
 ORDER BY id;
