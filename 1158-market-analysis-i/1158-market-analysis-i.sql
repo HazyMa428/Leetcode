c830184d83bf0412cd924a06c6931f7213b1a179
HazyMa428 <110778494+HazyMa428@users.noreply.github.com>
2022-11-09 08:20:29 -0500

    Time: 1107 ms (99.52%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -2,9 +2,11 @@
 
 
 
-SELECT u.user_id AS buyer_id, join_date, COUNT(order_date) AS orders_in_2019 
-FROM Users as u
-LEFT JOIN Orders as o
-ON u.user_id = o.buyer_id
-AND YEAR(order_date) = '2019'
-GROUP BY u.user_id
+
+
+select u.user_id as buyer_id, join_date, count(o.order_date) as orders_in_2019
+from users as u
+left join orders as o 
+on u.user_id = o.buyer_id
+and year(order_date) = '2019'
+group by u.user_id;
