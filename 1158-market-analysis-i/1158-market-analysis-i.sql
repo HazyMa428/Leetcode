69e274ecdd90f3a02f63760a92359d7b101607e4
HazyMa428 <110778494+HazyMa428@users.noreply.github.com>
2022-11-09 08:21:09 -0500

    Time: 1358 ms (70.74%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -4,7 +4,7 @@
 
 
 
-select user_id as buyer_id, join_date, count(o.order_date) as orders_in_2019
+select u.user_id as buyer_id, u.join_date, count(o.order_date) as orders_in_2019
 from users u
 left join orders o 
 on u.user_id = o.buyer_id
