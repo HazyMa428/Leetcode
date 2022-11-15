bcc1fcccb950eb64fcd6d584810ae2c847fb20ed
HazyMa428 <110778494+HazyMa428@users.noreply.github.com>
2022-11-15 07:34:58 -0500

    Time: 750 ms (64.19%), Space: 0B (100.00%) - LeetHub

--- 1112-highest-grade-for-each-student/1112-highest-grade-for-each-student.sql
+++ 1112-highest-grade-for-each-student/1112-highest-grade-for-each-student.sql
@@ -1,10 +1,8 @@
 # Write your MySQL query statement below
 
-SELECT student_id, MIN(course_id) AS course_id, grade
-FROM Enrollments
-WHERE (student_id, grade) IN
-(SELECT student_id, MAX(grade)
-FROM Enrollments
-GROUP BY student_id)
-GROUP BY student_id, grade
-ORDER BY student_id
+WITH P AS (SELECT *, ROW_NUMBER() OVER(PARTITION BY student_id ORDER BY grade DESC, course_id) AS grader
+           FROM Enrollments)
+           
+SELECT student_id, course_id, grade
+FROM P
+WHERE grader = 1;
