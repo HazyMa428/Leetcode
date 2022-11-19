2db30c9abdadca13aa36b37dbaf2880e9e51b2c7
HazyMa428 <110778494+HazyMa428@users.noreply.github.com>
2022-11-19 08:03:30 -0500

    Time: 710 ms (58.82%), Space: 0B (100.00%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,29 +1,6 @@
 # Write your MySQL query statement below
 
-
-SELECT DISTINCT
-  customer_id
-FROM
-  Customer
-WHERE
-  customer_id NOT IN (
-    SELECT
-      customer_id
-    FROM
-      (
-        SELECT DISTINCT
-          Customer.customer_id,
-          Product.product_key
-        FROM
-          Customer,
-          Product
-      ) AS AllPossibleCases
-    WHERE
-      (customer_id, product_key) NOT IN (
-        SELECT
-          customer_id,
-          product_key
-        FROM
-          Customer
-      )
-  );
+select customer_id
+from customer c
+group by customer_id
+having count(distinct product_key)=(select count(distinct product_key) from product)
